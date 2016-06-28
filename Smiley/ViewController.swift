50e3ca870bdfcddebe01ec0e177840b69169391f
Derrick McMillen <dj@mcmillen.net>
2016-06-28 08:03:10 -0700

    Initial Commit

---
+++ Smiley/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smiley
+//
+//  Created by Derrick McMillen on 6/28/16.
+//  Copyright © 2016 demc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
